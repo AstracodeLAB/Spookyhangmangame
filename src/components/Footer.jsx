3e1ba383e2bc23a282bf96bb87383b369876641b
AstracodeLAB <astracodelab@gmail.com>
2024-10-10 13:03:32 +0200

    Add links to social icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,16 +8,16 @@ const Footer = () => {
 			<div className='footer__content'>
 				<p>¿Necesitas una web?</p>
 				<div className='footer__socials'>
-					<a href='https://www.linkedin.com' target='_blank' rel='noopener noreferrer'>
+					<a href='https://www.linkedin.com/company/astracodelab/' target='_blank' rel='noopener noreferrer'>
           <IconBrandLinkedin stroke={2} />
 					</a>
-					<a href='https://www.instagram.com' target='_blank' rel='noopener noreferrer'>
+					<a href='https://www.instagram.com/astracodelab/' target='_blank' rel='noopener noreferrer'>
           <IconBrandInstagram stroke={2} />
 					</a>
-					<a href='https://www.facebook.com' target='_blank' rel='noopener noreferrer'>
+					<a href='https://m.facebook.com/people/Astracodelab/61564205213789/' target='_blank' rel='noopener noreferrer'>
           <IconBrandFacebook stroke={2} />
 					</a>
-					<a href='https://www.threads.net' target='_blank' rel='noopener noreferrer'>
+					<a href='https://www.threads.net/@astracodelab' target='_blank' rel='noopener noreferrer'>
           <IconBrandThreads stroke={2} />
 					</a>
 				</div>
