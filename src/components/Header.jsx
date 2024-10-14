a68a2bdb425abcc0092e2fe296fb9d4bdfd66445
AstracodeLAB <astracodelab@gmail.com>
2024-10-14 18:48:37 +0200

    Add instructions

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,16 @@
 const Header = (props) => {
-  return (
-    <header className="header">
-      <img className="logo" src="./Whitelogolab.png"></img>
-      <h1 className="header__title">{props.text}</h1>
-    </header>
-  );
+	return (
+		<header className='header'>
+			<img className='logo' src='./Whitelogolab.png'></img>
+			<h1 className='header__title'>{props.text}</h1>
+			<div className='header__intro'>
+				<p className='header__paragraph'>
+					Te retamos a adivinar películas de miedo en este juego del ahorcado (versión Halloween).
+				</p>
+				<p className='header__paragraph'>Cada día una nueva peli y una sorpresa especial si la adivinas...</p>
+			</div>
+		</header>
+	);
 };
 
 export default Header;
