80ba9dd2b437c88c29cc02f62f627901a4d25401
AstracodeLAB <astracodelab@gmail.com>
2024-10-15 12:27:16 +0200

    Add special text for mobile devices

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,11 @@ const Header = (props) => {
       <img className="logo" src="./Whitelogolab.png"></img>
       <h1 className="header__title">{props.text}</h1>
       <div className='header__intro'>
-				<p className='header__paragraph'>
+				<p className='header__paragraph header__paragraph--1'>
 					Te retamos a adivinar películas de miedo en este juego del ahorcado (versión Halloween).
 				</p>
-				<p className='header__paragraph'>Cada día una nueva peli y una sorpresa especial si la adivinas...</p>
+				<p className='header__paragraph header__paragraph--2'>Cada día una nueva peli y una sorpresa especial si la adivinas...</p>
+				<p className='header__paragraph header__paragraph--3'>¡Juega al ahorcado descubriendo cada día una nueva peli de miedo y gana una sorpresa!</p>
 			</div>
     </header>
   );
