27bcd6bc7be0c29d8b3b690457560033e8217e9f
AstracodeLAB <astracodelab@gmail.com>
2024-10-15 12:13:52 +0200

    Add padding-top to logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,12 @@ const Header = (props) => {
     <header className="header">
       <img className="logo" src="./Whitelogolab.png"></img>
       <h1 className="header__title">{props.text}</h1>
+      <div className='header__intro'>
+				<p className='header__paragraph'>
+					Te retamos a adivinar películas de miedo en este juego del ahorcado (versión Halloween).
+				</p>
+				<p className='header__paragraph'>Cada día una nueva peli y una sorpresa especial si la adivinas...</p>
+			</div>
     </header>
   );
 };
