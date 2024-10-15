a9a76aac442c607357ab5cc7b3c1e76e4a341d13
AstracodeLAB <astracodelab@gmail.com>
2024-10-15 13:33:34 +0200

    Addapt winner message content for mobile devices

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -11,19 +11,19 @@ const Modal = ({ showModal, closeModal, todayMovie }) => {
       <div className="modal-content">
         <div className="skeleton">
           <img className="cat" src="cat-halloween.gif" alt="gato negro con calabaza"/>
-          <p className="message-win">{todayMovie.message_win}</p>
+          <p className="message-win">{todayMovie.message_win_mobile}</p>
           </div>
         <div>
           <p className="message-astra">Como recompensa, tienes un <span className="green">10% de descuento</span> en nuestro servicio de portfolio*.</p>
-          <p>¡Esta es tu oportunidad para destacar como un verdadero superviviente del terror!</p>
+          <p className="message-big">¡Esta es tu oportunidad para destacar como un verdadero superviviente del terror!</p>
           <p className="message-astra-advice">*Oferta válida hasta el 1 de noviembre de 2024 con el <span className="green">código ASTRACODE</span></p>
         </div>
         <div className="movie-content">
           <img className="image-movie" src={todayMovie.image_win} alt="Placeholder" />
         <div className="movie-description">
-          <h2>{todayMovie.title_win} ({todayMovie.year_win})</h2>
-          <span>Director: {todayMovie.director_win}</span>
-          <span>{todayMovie.description_win}</span>
+          <h2 className="movie-title">{todayMovie.title_win} ({todayMovie.year_win})</h2>
+          <span className="movie-director">Director: {todayMovie.director_win}</span>
+          <span className="movie-info">{todayMovie.description_win}</span>
         </div>
        </div>
         <div className="modal-buttons">
